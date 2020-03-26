d4a938a49ea164bb77c8cfa9d75d3d76cb046f15
viveknagesh1 <56115917+viveknagesh1@users.noreply.github.com>
2020-03-26 19:43:06 +0530

    Forgot password implemented.

--- app/src/main/java/com/example/carbookingappfinal/Login.java
+++ app/src/main/java/com/example/carbookingappfinal/Login.java
@@ -1,8 +1,11 @@
 package com.example.carbookingappfinal;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,6 +17,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,10 +27,11 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Login extends AppCompatActivity {
     EditText mEmail,mPassword;
     Button mLoginBtn;
-    TextView mCreateBtn;
+    TextView mCreateBtn,forgotPasswordLink;
     ProgressBar progressBar;
     FirebaseAuth fAuth;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +43,7 @@ public class Login extends AppCompatActivity {
         fAuth = FirebaseAuth.getInstance();
         mLoginBtn = findViewById(R.id.loginBtn);
         mCreateBtn = findViewById(R.id.createText);
+        forgotPasswordLink=findViewById(R.id.forgot);
 
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,6 +88,42 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        forgotPasswordLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText resetMail=new EditText(v.getContext());
+                final AlertDialog.Builder passwordResetBuilder=new AlertDialog.Builder(v.getContext());
+                passwordResetBuilder.setTitle("Reset Password ?");
+                passwordResetBuilder.setMessage("Enter you email to recieve link to change password");
+                passwordResetBuilder.setView(resetMail);
+                passwordResetBuilder.setPositiveButton("yes ", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String mail=resetMail.getText().toString();
+                        fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Login.this,"Password change link sent successfully",Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Login.this,"Reset Link was not sent "+e.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                });
+                passwordResetBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+            }
+        });
+
+
 
 
         mCreateBtn.setOnClickListener(new View.OnClickListener() {
@@ -92,4 +135,6 @@ public class Login extends AppCompatActivity {
 
 
     }
+
+
 }
